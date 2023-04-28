c786edcb23a038fa9613ca31768d43cbb77a24cc
Richard <richard@pesapal.com>
2023-04-28 20:40:38 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Richard kamere on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
